2f0868d4df777ceb1f27a5e6d36c300de4728978
tomhoon <gnsdl9108@naver.com>
2025-08-10 22:07:55 +0900

    fix: cors 수정

--- src/main/java/com/hong/tms/config/WebConfig.java
+++ src/main/java/com/hong/tms/config/WebConfig.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
@@ -22,5 +24,13 @@ public class WebConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", config);
     return source;
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*") // allow all domains
+        .allowedMethods("*") // allow all methods
+        .allowedHeaders("*"); // allow all headers
   }
 }
